68e995fdf67dd32c96f90c2052d8beb1bcbe6abe
Purecon <mfurqon2001@gmail.com>
2022-09-17 16:25:55 +0700

    Fix convolution and change input

--- convolution.py
+++ convolution.py
@@ -2,24 +2,21 @@ from math import floor
 import numpy as np
 
 class ConvolutionStep:
-    def __init__(self, banyak_input, input_h, input_w, input_pad, banyak_filter, filter_size, input_stride):
-        self.banyak_input = banyak_input
+    def __init__(self,input_matrixes, input_pad, banyak_filter, filter_size, input_stride):
+        self.banyak_input = len(input_matrixes)
         #NOTE: input untuk h dan w 
-        self.input_h = input_h
-        self.input_w = input_w
         self.input_pad = input_pad
         self.banyak_filter = banyak_filter
         self.filter_size = filter_size
         self.input_stride = input_stride
 
         #Input matrixes
+        self.input_matrixes = input_matrixes
         input_matrixes = []
-        for i in range(banyak_input):
-            #Randomize for testing 
-            random_matrix = np.random.randint(0,10,(input_h,input_w))
+        for matrix in self.input_matrixes:
             #Padding
             #input_matrixes.append(np.pad(random_matrix,input_pad, mode='empty').tolist())
-            input_matrixes.append(np.pad(random_matrix,input_pad,'constant', constant_values=(0)))
+            input_matrixes.append(np.pad(matrix,input_pad,'constant', constant_values=(0)))
         self.input_matrixes = input_matrixes
 
         #Filter kernel matrixes
@@ -33,31 +30,39 @@ class ConvolutionStep:
     def convolution(self):
         output=[]
         for matriks in self.input_matrixes:
-            i = 0
+            v_h= floor((len(matriks[0])-self.filter_size)/self.input_stride + 1)
+            v_w = floor((len(matriks)-self.filter_size)/self.input_stride + 1)
+            h = len(matriks[0])-self.filter_size
+            w = len(matriks)-self.filter_size
+
             output_matrixes = []
-            output_matrix=[]
-            for indeks_kernel in range(len(self.kernel_matrixes)):
-                while i <  self.input_w+self.input_pad:
+            for kernel_matriks in self.kernel_matrixes:
+                v_w_counter = 0
+                i = 0
+                output_matrix=[]
+                while v_w_counter < v_w:
+                    v_h_counter = 0
                     j=0
                     output_row = []
-                    while j < self.input_h+self.input_pad:
+                    while v_h_counter < v_h:
                         i_filter = 0
                         conv_sum = 0
                         #convolution
                         for i2 in range(i,i+self.filter_size):
                             j_filter = 0
                             for j2 in range(j,j+self.filter_size):
-                                conv_sum += matriks[i2][j2] * self.kernel_matrixes[indeks_kernel][i_filter][j_filter]
+                                conv_sum += matriks[i2][j2] * kernel_matriks[i_filter][j_filter]
                                 j_filter+=1
                             i_filter+=1
                         output_row.append(conv_sum)
                         j+=self.input_stride
+                        v_h_counter+=1
                     output_matrix.append(output_row)
                     i+=self.input_stride
+                    v_w_counter+=1
                 output_matrixes.append(output_matrix)
             output.append(output_matrixes)
         self.output = output
-        return output
 
 def test():
     #Ukuran input 
@@ -79,15 +84,21 @@ def test():
     V_height = (input_h-filter_size+2*input_pad)/input_stride + 1
     print('Expected output {0} and {1}'.format(floor(V_width),floor(V_height)))
 
+    input_matrixes = []
+    for i in range(banyak_input):
+        #Randomize for testing 
+        input_matrixes.append(np.random.randint(0,10,(input_h,input_w)))
+
     #Object
-    convolutionStepTest = ConvolutionStep(banyak_input, input_h, input_w, input_pad, banyak_filter, filter_size,input_stride)
+    convolutionStepTest = ConvolutionStep(input_matrixes, input_pad, banyak_filter, filter_size,input_stride)
 
     #Print for debug
     print_matrix("Input matriks", convolutionStepTest.input_matrixes)
     print_matrix("Kernel matriks", convolutionStepTest.kernel_matrixes)
 
     #Convolution output
-    output = convolutionStepTest.convolution()
+    convolutionStepTest.convolution()
+    output = convolutionStepTest.output
     for o_matrixes in output:
         print_matrix("Output matriks", o_matrixes)
 
