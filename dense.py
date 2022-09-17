d789dfddcd3910d6559e30e3112cdf854f49758c
LastAeon <syihabuddiny@gmail.com>
2022-09-17 20:25:26 +0700

    feat: wrap FFNN in dense class

---
+++ dense.py
@@ -0,0 +1,9 @@
+from FFNN import FFNN
+from flatten import flatten
+
+class dense:
+    def __init__(self, filename):
+        self.ffnn = FFNN(filename)
+    
+    def predict_set_of_matrix(self, set_of_matrix):
+        return self.ffnn.predict(flatten(set_of_matrix))
