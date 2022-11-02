fd05ee688e80e922083d8f5cdf56c1c9c7ad81c2
Purecon <mfurqon2001@gmail.com>
2022-11-02 18:13:34 +0700

    Add cell class

--- rnn_gate.py
+++ rnn_gate.py
@@ -1,6 +1,85 @@
 from activation_function import sigmoid
 import numpy as np
 
+class Cell:
+    #rand_initialize(bool): parameter randomized 
+    #U(list of matrix): kumpulan matriks U
+    #f: forgot gate weight & bias
+    #i: input gate weight & bias
+    #c: cell state weight & bias
+    #o: output gate weight & bias
+    def __init__(self, rand_initialize, U, f, i, c, o):
+        #Not implemented input size
+
+        if(rand_initialize == False):
+            self.Uf = U[0]
+            self.Ui = U[1]
+            self.Uc = U[2]
+            self.Uo = U[3]
+            
+            self.Wf = f[0]
+            self.bf = f[1]
+            self.Wi = i[0]
+            self.bi = i[1]
+            self.Wc = c[0]
+            self.bc = c[1]
+            self.Wo = o[0]
+            self.bo = o[1]
+
+            self.cprev = 0
+            self.hprev = 0
+        else:
+            print("Not Implemented")
+    
+    def calculate_timestep(self,x,verbose):
+        ft = self.calculate_forgot(Uf, x, Wf, self.hprev, bf)
+        it = self.calculate_input(Ui, x, Wi, self.hprev, bi)
+        candidate = self.calculate_candidate(Uc, x, Wc, self.hprev, bc)
+        ct = self.calculate_cell(ft, self.cprev, it, candidate)
+        ot = self.calculate_output(Uo, x, Wo, self.hprev, bo)
+        ht = self.calculate_hidden(ot,ct)
+
+        if(verbose):
+            print("ht: ",ht)
+            print("ft: ",ft)
+            print("it: ",it)
+            print("c~: ",candidate)
+            print("ct: ",ct)
+            print("ot: ",ot)
+            print("Timestep ct {} dan ht {}".format(ct,ht))
+        
+        self.cprev = ct
+        self.hprev = ht
+        return ct,ht
+
+    #Gate sigmoid
+    def calculate_gate(self,u: list[float], x: list[float], w: float, h: float, b:float):
+        return sigmoid(np.dot(u, x) + w * h + b)
+
+    #Forgot gate
+    def calculate_forgot(self,uf: list[float], x: list[float], wf: float, hprev: float, bf:float):
+        return self.calculate_gate(uf, x, wf, hprev, bf)
+
+    #Input gate
+    def calculate_input(self,ui: list[float], x: list[float], wi: float, hprev: float, bi:float):
+        return self.calculate_gate(ui, x, wi, hprev, bi)
+
+    #Calculate candidate
+    def calculate_candidate(self,uc: list[float], x: list[float], wc: float, hprev: float, bc:float):
+        return np.tanh(np.dot(uc, x) + wc * hprev + bc)
+
+    #Cell state
+    def calculate_cell(self,ft: float, cprev:float, it: float, candidate:float):
+        return ft*cprev + it*candidate
+
+    #Output gate
+    def calculate_output(self,uo: list[float], x: list[float], wo: float, hprev: float, bo:float):
+        return self.calculate_gate(uo,x,wo,hprev,bo)
+
+    def calculate_hidden(self,ot:float, ct:float):
+        return ot * np.tanh(ct)
+
+#TEST class
 X = [
     [1, 2],
     [.5, 3]
@@ -19,47 +98,7 @@ Wi, bi = .8, .65
 Wc, bc = .15, .2
 Wo, bo = .25, .1
 
-#Gate sigmoid
-def calculate_gate(u: list[float], x: list[float], w: float, h: float, b:float):
-    return sigmoid(np.dot(u, x) + w * h + b)
-
-#Forgot gate
-def calculate_forgot(uf: list[float], x: list[float], wf: float, hprev: float, bf:float):
-    return calculate_gate(uf, x, wf, hprev, bf)
-
-#Input gate
-def calculate_input(ui: list[float], x: list[float], wi: float, hprev: float, bi:float):
-    return calculate_gate(ui, x, wi, hprev, bi)
-
-#Calculate candidate
-def calculate_candidate(uc: list[float], x: list[float], wc: float, hprev: float, bc:float):
-    return np.tanh(np.dot(uc, x) + wc * hprev + bc)
-
-#Cell state
-def calculate_cell(ft: float, cprev:float, it: float, candidate:float):
-    return ft*cprev + it*candidate
-
-#Output gate
-def calculate_output(uo: list[float], x: list[float], wo: float, hprev: float, bo:float):
-    return calculate_gate(uo,x,wo,hprev,bo)
-
-def calculate_hidden(ot:float, ct:float):
-    return ot * np.tanh(ct)
-
-#print(calculate_gate(Uf, X[0], Wf, hprev, bf))
-#TEST
-#X timestep 1
-# x1 = X[0]
-# ft = calculate_forgot(Uf, x1, Wf, hprev, bf)
-# print("ft: ",ft)
-# it = calculate_input(Ui, x1, Wi, hprev, bi)
-# print("it: ",it)
-# candidate = calculate_candidate(Uc, x1, Wc, hprev, bc)
-# print("C~: ",candidate)
-# ct = calculate_cell(ft, Cprev, it, candidate)
-# print("ct: ",ct)
-# ot = calculate_output(Uo, x1, Wo, hprev, bo)
-# print("ot: ",ot)
-# ht = calculate_hidden(ot,ct)
-# print("ht: ",ht)
-# print("Timestep 1 ct {} dan ht {}".format(ct,ht))
+testCell = Cell(False,[Uf,Ui,Uc,Uo],[Wf,bf],[Wi,bi],[Wc,bc],[Wo,bo])
+for x in X:
+    hasil = testCell.calculate_timestep(x,verbose=True)
+    print(hasil)
